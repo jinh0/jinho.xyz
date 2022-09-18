8d45a2165cb5cdeccedfd4986f8b462e779d7932
Jinho <yoonjinho0318@gmail.com>
2022-09-17 23:07:56 -0400

    feat: add padding to title

--- components/Title.tsx
+++ components/Title.tsx
@@ -1,5 +1,5 @@
 const Title = ({ children }: { children: JSX.Element | string }) => {
-  return <p className="mt-3 mb-6 text-5xl font-bold">{children}</p>
+  return <p className="mt-6 mb-6 text-5xl font-bold">{children}</p>
 }
 
 export default Title
